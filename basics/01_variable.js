6bfac9cd5bd88276ced351e103dbea17a8cdd6c3
abdullahProfile <abdullahdew88@gmail.com>
2025-04-23 02:06:33 +0500

    day 01 basics

---
+++ basics/01_variable.js
@@ -0,0 +1,9 @@
+const userId="1234"; //unchangeable
+
+let userName="Abdullah"; //block scope
+
+var userPassword="12313"; //funtional scope
+
+console.log(userId); //print the variable
+
+console.table([userId,userName,userPassword]); //print in table form
---
+++ basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+//data types
+
+//Number
+
+let age=21;
+let price=99.2;
+
+//String
+
+let name="Abdullah";
+let greeting="hello";
+let message=`hi ${name}`;
+
+//boolean
+
+let isOnline=true;
+let isLogin=false;
+
+//undefine;
+let unknown;
+
+//null
+
+let data=null; //intentionally empty
+
+//object
+
+let user= {
+  name:"Abdullah",
+  age:22
+}
+
+console.table([age,price,name,greeting,message,isOnline,isLogin,unknown,data,user]);
