b6aee94d923a32b7c190e2f07be46d89e93bbeb4
abdullahProfile <abdullahdew88@gmail.com>
2025-04-29 01:37:31 +0500

    Day 6 :Learn About Premitive and Non Premitive data types

---
+++ basics/05_PremitiveAndNonePrimitive.js
@@ -0,0 +1,72 @@
+// Premitive data types: call  by value
+
+/*
+ 1: String
+ 2:number
+ 3: null
+ 4: undifined
+ 5: Symbol
+ 6: BigInt
+ 7:Boolean
+*/
+
+// Examples:
+
+const score=100; //number
+
+let isLoging=true; //boolean
+
+let MyName="Abdullah" //string
+
+let BigNumber=1229121821;//big Int
+
+let IsNull= null;
+
+let IsUndefined;//value not assigned
+
+// 2: Example of Premitive data type
+
+let isPremitive= 10;
+
+function changeValue(x) {
+  x = 20;
+}
+
+changeValue(isPremitive);//function gets so change in the x is does not effect a
+console.log(isPremitive); //output is 10
+
+//Non Premitive data types: call by refrence
+
+/*
+A reference (address) to the original data is passed.
+
+Changes made affect the original data.
+
+Example: Passing non-primitive data types (like object, array).
+
+ */
+
+let isArray={id:1,name:"Abdullah"};
+
+function changeArray(o){
+  o.name="Muhammad Abudllah"
+}
+
+changeArray(isArray);
+console.log(isArray.name);  // Output: Muhammad Abdullah
+console.log(isArray);
+
+//Example of Object
+
+let isPerson={
+  name: "Abdullah khan",
+  age:20
+};
+
+function RecieveObject(obj){
+  obj.name="Abdullah malik";
+  obj.age=21;
+}
+
+RecieveObject(isPerson); //modify the object
+console.log(isPerson); //resulut is Abdullah malik age: 21
