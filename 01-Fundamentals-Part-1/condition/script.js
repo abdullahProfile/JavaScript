d4af752cbd9af29a8874f2538f4e84a133d09596
abdullahProfile <abdullahdew88@gmail.com>
2025-06-21 00:31:48 +0500

    conditions

---
+++ 01-Fundamentals-Part-1/condition/script.js
@@ -0,0 +1,24 @@
+
+const age= 15;
+
+if(age >= 18){
+  console.log("Your are allow to drive 🚗")
+}else {
+  const yearsLeft=18 -age;
+
+  console.log(`Your are too young,wait another  ${yearsLeft} years`);
+}
+
+
+
+let userAge=(parseInt(prompt("Enter Your age: ")));
+
+if(userAge >= 18){
+  alert("Your are able to start driving");
+}else {
+  const leftYears=18 - userAge;
+
+  alert(`You are to young, wait another ${leftYears} years`);
+}
+
+
--- 01-Fundamentals-Part-1/guessNumber/script.js
+++ 01-Fundamentals-Part-1/guessNumber/script.js
@@ -4,16 +4,16 @@ let guess;
 
 while (guess !== secretNumber){
 
- console.log(guess=parseInt(prompt("Guess a number between 1 and 100")));
+ console.log(guess=parseInt(prompt("Guess a number between 1 and 100 😎")));
 
  if (isNaN(guess) || guess < 1 || guess > 100){
-  alert("please enter valid number between 1 and 100");
+  alert("please enter valid number between 1 and 100 ❌");
  }
   if (guess < secretNumber){
-    alert("To low")
+    alert("To low 👇")
   } else if(guess > secretNumber){
-    alert("To heigh");
+    alert("To heigh ✈️");
   }else {
-    alert("Great You guess the Number " + guess);
+    alert("Great You guess the Number 🎉" + guess);
   }
 }
