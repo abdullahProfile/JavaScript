101fdf8a4ab02463e814c04581b45377005aff78
abdullahProfile <abdullahdew88@gmail.com>
2025-06-20 23:33:42 +0500

    operators

---
+++ 01-Fundamentals-Part-1/operators/script.js
@@ -0,0 +1,19 @@
+//operators in js
+
+//subtraction
+
+const ageAbdullah=21-11;
+
+const ageHamza=40-20;
+
+
+console.log(ageAbdullah,ageHamza);
+
+console.log(ageAbdullah * 30, ageHamza * 10, 10**2);
+
+//addition
+
+const firstName="Muhammad";
+const secondName="Abdullah"
+
+console.log(firstName + secondName);
